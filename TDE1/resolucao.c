ebbf8ae17cce5c556714c9a87007db26e0577cab
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2020-09-17 12:48:34 -0300

    Verificador de dígito do CPF 100% funcional

--- TDE1/resolucao.c
+++ TDE1/resolucao.c
@@ -1,17 +1,36 @@
 // Cálculo dos Dígitos Verificadores do CPF
-
+#define TAM_CPF 11
 #include <stdio.h>
+#include <string.h>
+
+void imprimirLinha(tamanho) {
+  int i;
+  for (i = 0; i < tamanho; i++) {
+    printf("-");
+  }
+  printf("\n");
+}
 
 int main() {
-  int cpf[15], cpfValido[15];
+  char *cpfChar;
+  int cpf[TAM_CPF], cpfValido[TAM_CPF];
   int *ponteiro = cpf;
-  int fator, soma = 0, valido = 0, i;
+  int fator, soma = 0, valido = 0, i, pos = 0, tamCpfInserido;
 
-  for (i = 0; i < 11; i++) {
-    printf("Insira o %d digito do cpf: ", i + 1);
-    scanf("%d", &cpf[i]);
-    if (i <= 8) {
-      cpfValido[i] = cpf[i];
+  do {
+    printf("Insira o cpf: ");
+    scanf("%s", cpfChar);
+    tamCpfInserido = (int) strlen(cpfChar);
+  } while(tamCpfInserido != TAM_CPF && tamCpfInserido != TAM_CPF + 3);
+  printf("\n");
+
+  for (i = 0; i <= tamCpfInserido - 1; i++) {
+    if (48 <= cpfChar[i] && cpfChar[i] <= 57) {
+      cpf[pos] = cpfChar[i] - 48;
+      if (pos <= 8) {
+        cpfValido[pos] = cpf[pos];
+      }
+      pos++;
     }
   }
 
@@ -20,13 +39,10 @@ int main() {
     ponteiro++;
   }
 
-  printf("Soma = %d\n", soma);
-  printf("Valor 1º digito = %d\n", *ponteiro);
-
-  if (soma % 11 < 2) {
+  if (soma % TAM_CPF < 2) {
     cpfValido[9] = 0;
   } else {
-    cpfValido[9] = 11 - (soma % 11);
+    cpfValido[9] = TAM_CPF - (soma % TAM_CPF);
   }
 
   if (cpfValido[9] == *ponteiro) {
@@ -36,18 +52,15 @@ int main() {
   }
   ponteiro = cpf;
   soma = 0;
-  for (fator = 11; fator >= 2; fator--) {
+  for (fator = TAM_CPF; fator >= 2; fator--) {
     soma += *ponteiro * fator;
     ponteiro++;
   }
 
-  printf("Soma = %d\n", soma);
-  printf("Valor 2º digito = %d\n", *ponteiro);
-
-  if (soma % 11 < 2) {
-    cpfValido[9] = 0;
+  if (soma % TAM_CPF < 2) {
+    cpfValido[10] = 0;
   } else {
-    cpfValido[9] = 11 - (soma % 11);
+    cpfValido[10] = TAM_CPF - (soma % TAM_CPF);
   }
 
   if (cpfValido[9] == *ponteiro) {
@@ -56,22 +69,29 @@ int main() {
     valido = 0;
   }
 
-  if (soma % 11 < 2 && *ponteiro == 0) {
+  if (soma % TAM_CPF < 2 && *ponteiro == 0) {
     valido = 1;
-  } else if (*ponteiro == 11 - (soma % 11)) {
+  } else if (*ponteiro == TAM_CPF - (soma % TAM_CPF)) {
     valido = 1;
   } else {
     valido = 0;
   }
 
+  imprimirLinha(TAM_CPF * 3);
+  printf("O CPF digitado é ");
   if (valido) {
-    printf("CPF válido\n");
+    printf("\033[1;32m");
+    printf("VÁLIDO\n");
+    printf("\033[0m");
   } else {
-    printf("CPF inválido\n");
+    printf("\033[1;31m");
+    printf("INVÁLIDO\n");
+    printf("\033[0m");
   }
+  imprimirLinha(TAM_CPF * 3);
 
   printf("CPF digitado -> ");
-  for (i = 0; i < 14; i++) {
+  for (i = 0; i <= TAM_CPF - 1; i++) {
     printf("%d", cpf[i]);
     if ((i + 1) % 9 == 0) {
       printf("-");
@@ -80,7 +100,7 @@ int main() {
     }
   }
   printf("\nCPF válido -> ");
-  for (i = 0; i < 14; i++) {
+  for (i = 0; i <= TAM_CPF - 1; i++) {
     printf("%d", cpfValido[i]);
     if ((i + 1) % 9 == 0) {
       printf("-");
@@ -89,6 +109,7 @@ int main() {
     }
   }
   printf("\n");
+  imprimirLinha(TAM_CPF * 3);
   
   return 0;
 }
